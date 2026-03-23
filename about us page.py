0f34508de43162561980f44d9d2072685e68dc18
andrewwangdarkness1105 <andrewwangpiano@gmail.com>
2026-03-23 13:28:07 +1100

    Delete about us page.py

--- about us page.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-st.title("About Us")
-st.subheader("Learn More About Us")
-st.write("We are a group of four people from an Australian team called Institute who have designed and coded this app as a part of our project for the 2025-2026 Unearthed First Lego League Challenge. Our hope is that this website can save archeologists save time publishing what they learnt on an archeological dig site more efficiently.")
-
-st.image("Proto1 innovation/institute logo.png", caption=None, width= 325, use_column_width=None, clamp=False, channels="RGB", output_format="auto", use_container_width=None)
-
